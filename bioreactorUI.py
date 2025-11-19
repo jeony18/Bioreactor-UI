0751731be3d85b05067527990c737a99bb2ed581
jeony18 <zcabjeo@ucl.ac.uk>
2025-11-19 12:44:53 +0000

    redid frames

--- bioreactorUI.py
+++ bioreactorUI.py
@@ -5,31 +5,30 @@ main = tk.Frame(root)
 root.geometry("1280x720")
 
 
-# Root grid: 1 row, 2 columns
-root.grid_rowconfigure(0, weight=1)
+#Top and bottom sections 2:1
+root.grid_rowconfigure(0, weight=2)
+root.grid_rowconfigure(1, weight=1)  
 root.grid_columnconfigure(0, weight=1) 
-root.grid_columnconfigure(1, weight=2)  
 
-# Frames
-leftFrame = tk.Frame(root, bg="lightgray")
-rightFrame = tk.Frame(root, bg="lightblue")
-
-leftFrame.grid(row=0, column=0, sticky="nesw")
-rightFrame.grid(row=0, column=1, sticky="nesw")
-
-# Inside leftFrame: 2 rows stacked vertically
-leftFrame.grid_rowconfigure(0, weight=1)
-leftFrame.grid_rowconfigure(1, weight=1)
-leftFrame.grid_rowconfigure(2, weight = 1)
-leftFrame.grid_columnconfigure(0, weight=1)
-
-topLeft = tk.Frame(leftFrame, bg="pink")
-mid = tk.Frame(leftFrame, bg = "black")
-bottomLeft = tk.Frame(leftFrame, bg="orange")
+topFrame = tk.Frame(root, bg="lightgray")
+botFrame = tk.Frame(root, bg="lightblue")
 
 
-topLeft.grid(row=0, column=0, sticky="nesw")
-mid.grid(row = 1, column = 0)
-bottomLeft.grid(row=2, column=0, sticky="nesw")
+topFrame.grid(row=0, column=0, sticky="nesw")
+botFrame.grid(row=1, column=0, sticky="nesw")
+
+botFrame.grid_columnconfigure(0, weight=1)
+botFrame.grid_columnconfigure(1, weight=1)
+botFrame.grid_columnconfigure(2, weight = 1)
+botFrame.grid_rowconfigure(0, weight=1)
+
+botLeft = tk.Frame(botFrame, bg="pink")
+botMid = tk.Frame(botFrame, bg = "red")
+botRight = tk.Frame(botFrame, bg="orange")
+
+
+botLeft.grid(row=0, column=0, sticky="nesw")
+botMid.grid(row = 0, column = 1, sticky = "nesw")
+botRight.grid(row=0, column=2, sticky="nesw")
 
 root.mainloop()
